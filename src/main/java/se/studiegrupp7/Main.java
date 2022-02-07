404b4d1a377c2ab582333f7cf3901a4b2f39c96b
vikash123765 <92863236+vikash123765@users.noreply.github.com>
2022-02-07 05:19:58 -0800

    first commit

--- src/main/java/se/studiegrupp7/Main.java
+++ src/main/java/se/studiegrupp7/Main.java
@@ -1,8 +1,34 @@
 package se.studiegrupp7;
 
+import java.awt.*;
+import java.util.Scanner;
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
+
+
+
+   StartMenu startMenu = new StartMenu();
+        Scanner scanner = new Scanner(System.in);
+        startMenu.display();
+        int input = scanner.nextInt();
+        startMenu.startMenu(input);
+
+
+
+
+            HighScore highScore = new HighScore();
+            Scanner scanner2 = new Scanner(System.in);
+            highScore.DisplayScoreMenu();
+            int input2 = scanner.nextInt();
+           highScore.ScoreMenu(input2);
+
+
+        }
+
 
     }
-}
+
+
+
