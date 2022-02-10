5f66c791289e503dd74df40d71dfb58ebc73ca39
vikash123765 <92863236+vikash123765@users.noreply.github.com>
2022-02-10 00:04:20 -0800

    ändringar
    senaste  02- 10-2022

--- src/main/java/se/studiegrupp7/Main.java
+++ src/main/java/se/studiegrupp7/Main.java
@@ -30,5 +30,7 @@ public class Main {
                 menuRunning = false;
             }
         }
+
+
     }
 }
---
+++ src/main/java/se/studiegrupp7/gameplay/Behaviors.java
@@ -0,0 +1,7 @@
+package se.studiegrupp7.gameplay;
+
+public interface Behaviors {
+
+    public void chooseBehavior();
+
+}
